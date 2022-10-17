19595fdddae85afa355b77d43586ef006592c612
ilaydakaraca <69599740+ilaydakaraca@users.noreply.github.com>
2022-10-17 12:30:07 +0300

    Add files via upload

--- deneme.py
+++ deneme.py
@@ -1,4 +1,4 @@
-__author__ = "Mehmet Değirmenci"
+__author__ = "Ilayd Karaca"
 
 
 import streamlit as st
@@ -11,22 +11,16 @@ from fastai.vision.all import *
 
 
 
-url = ("http://dl.dropboxusercontent.com/s/w20vwd3bu7fo4ut/best.pt?raw=1")
-filename = "best.pt"
+url = ("https://www.dropbox.com/s/drg59pqp7b56sf8/best%20%281%29.pt?dl=0")
+filename = "best (1).pt"
 urlretrieve(url,filename)
 
 
 
-urll = ("http://dl.dropboxusercontent.com/s/ecl4tj6q2u8s4q3/fig-03_5.png?raw=1")
-filenamee = "1.png"
-urlretrieve(urll,filenamee)
-st.image(filenamee)
-st.write('# LEUKEMIA DETECTION PLATFORM ')
 
 
 
-
-uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg'])
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg', 'jpg', 'tiff', 'tif'])
 
 
 
@@ -38,56 +32,12 @@ if uploaded_file is None:
 else:
     image = Image.open(uploaded_file)
     img_array = np.array(image)
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=filename)
+    model = torch.hub.load('ilaydakaraca/pore_detection', 'custom', path=filename)
     model.conf = 0.75
     #model.conf = st.sidebar.slider("Confidence threshold", 0.0, 1.0, 0.5, 0.01)
     
     model.results = model(img_array, size=512)
-    model.results.save("yolov5/results")
+    model.results.save("pore_detection/results")
     model.results.save()
-    st.image("yolov5/results/image0.jpg")
+    st.image("pore_detection/results/image0.jpg")
     ######
-   
-        
-        
-  
-        
-
-
-        
-
-        
-  
-
- #######
-       
- 
-    liste = []
-    liste1 = []
-    liste2 = []
-    liste0 = []
-
-
-    for i in model.results.xywh:
-        for j in i:
-            for k in j:
-                liste.append(k)
-            if k ==2:
-                liste2.append(k)
-            elif k == 1:
-                liste1.append(k)
-            elif k == 0:
-                liste0.append(k)
-
-
-    st.write("Number of cells detected:")
-    st.write("White Blood Cell:",len(liste0))
-    st.write("Red Blood Cell:",len(liste1))
-    st.write("Blast Cell",len(liste2))
-    
-    
-    
-
-
-
-
